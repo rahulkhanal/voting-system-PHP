f64df1957eda72dd2f8df473b074641241b9b7a7
Rahul <rahul39khanal@gmail.com>
2023-06-23 21:32:01 +0545

    initialization

---
+++ View/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ஜ۞ஜ मतदान ஜ۞ஜ</title>
+    <style>
+        h1 span {
+            color: red;
+        }
+
+        h1 {
+            color: blue;
+            border: 2px solid red;
+            width: 50%;
+            padding: 1rem;
+            font-size: 4rem;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1><span>ஜ۞ஜ</span> मतदान <span>ஜ۞ஜ</span></h1>
+    </center>
+</body>
+
+</html>
